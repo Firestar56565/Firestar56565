cb14b4411bcad89ad361bec660b16b2ee9cd87fc
Firestar56565 <34037661+Firestar56565@users.noreply.github.com>
2018-01-06 09:59:05 -0800

    Add files via upload

---
+++ circles/main.js
@@ -0,0 +1,10 @@
+var NUM_CIRCLES = 12;
+var circleDiameter;
+function setup() {
+  createCanvas(480, 600);
+  circleDiameter = width/NUM_CIRCLES
+}
+
+function draw() {
+  ellipse(width/2,height/2,circleDiameter,circleDiameter);
+}
